cd4eabb47f16d04d318b87317ffc81ff7f52bdf0
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-07-12 19:31:48 -0700

    re spaces matter.

--- LBNL_HI_Global_Signal/beams.py
+++ LBNL_HI_Global_Signal/beams.py
@@ -33,7 +33,7 @@ def convert_amp_phase_txt_to_uvbeam(
     # generate feed yaml
     filename_list = glob.glob(beam_folder + "*.txt")
     # extract frequencies
-    re_freq = re.compile("f=0.[0-9]{1, 3}")
+    re_freq = re.compile("f=0.[0-9]{1,3}")
     frequencies = [float(re_freq.findall(fname)[0].split("=")[-1]) * 1e9 for fname in filename_list]
     # sort filenames by frequencies
     filename_list = sorted(filenames, key=lambda x: frequencies[filenames.index(x)])
